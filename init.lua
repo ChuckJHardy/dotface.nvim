bdcf2680869e3fba76361e63653fe0296d0cde9b
Chuck J Hardy <github@chuckjhardy.com>
2024-05-04 22:34:18 +0100

    Quick Buffer Nav

--- init.lua
+++ init.lua
@@ -395,6 +395,8 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>bb', '<cmd>bprevious<CR>', { desc = 'Buffer [B]ack (Previous)' })
+      vim.keymap.set('n', '<leader>bn', '<cmd>bnext<CR>', { desc = 'Buffer [N]ext' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
