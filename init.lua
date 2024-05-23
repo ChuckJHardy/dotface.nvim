09ffea0b2277d8a902f3d171e1e6e093dac8e7f2
Chuck J Hardy <github@chuckjhardy.com>
2024-05-23 20:41:33 +0100

    Add ChatGPT

--- init.lua
+++ init.lua
@@ -277,6 +277,26 @@ require('lazy').setup({
     -- See Commands section for default commands if you want to lazy load on them
   },
 
+  -- ChatGPT
+  {
+    'jackMort/ChatGPT.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('chatgpt').setup {
+        api_key_cmd = 'pass Dev/ChatGPT',
+        openai_params = {
+          model = 'gpt-4o',
+        },
+      }
+    end,
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'folke/trouble.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+  },
+
   -- UI
   {
     'folke/noice.nvim',
